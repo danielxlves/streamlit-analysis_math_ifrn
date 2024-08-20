876f0a4df07e1ffb7133e85bd9f0998ad6098f98
danielxlves <daniel.nascimento.702@ufrn.edu.br>
2024-08-20 00:57:26 -0300

    feat: realizando os merges

--- analise.py
+++ analise.py
@@ -1,28 +1,49 @@
+
 import streamlit as st
 import pandas as pd
 import io
 import plotly.express as px
 
 def load_data():
-    dados_mergado = pd.read_json('dados_workflow_ivan.json')
-    return dados_mergado
+    #alunos = pd.read_json('aluno.json')
+    matricula = pd.read_json('MatriculaPeriodo.json')
+    notas = pd.read_json('Notas.json')
+    disciplina = pd.read_json('Disciplina.json')
+    situacao = pd.read_json('SituacaoMatriculaPeriodo.json')
+
+    return matricula, notas, disciplina, situacao
 
 def main():
-    dados_mergado = load_data()
-    matematica_i = dados_mergado["MatemticaI120H"].dropna()
 
-    st.title('Análise de dados da disciplina \"Matemática I\" dos cursos integrados do IFRN.')
+    matricula, notas, disciplina, situacao = load_data()
 
-    buffer = io.StringIO()
-    dados_mergado.info(buf=buffer)
-    s = buffer.getvalue()
-    st.code("Informações do dataset com os dados combinados:\n" + s)
+    #st.text("Aluno")
+    #st.dataframe(alunos)
+    #st.text("Matricula")
+    #st.dataframe(matricula)
+    #st.text("Notas")
+    #st.dataframe(notas)
+    #st.text("Disciplinas")
+    #st.dataframe(disciplina)
+    #st.text("Situacao")
+    #st.dataframe(situacao)
 
-    st.header('Series da disciplina de \"Matemática I\", filtrada dos dados combinados.')    
-    st.dataframe(matematica_i)
+    # Merges/Joins
+
+    merge_matricula_situacao = pd.merge(matricula, situacao, left_on='situacao_id', right_on='id', suffixes=('_matricula', '_situacao'))
     
-    fig = px.histogram(matematica_i, x=matematica_i, title='Distribuição de Notas em Matemática I')
-    st.plotly_chart(fig)
-        
+    st.text("Merge matricula -> situacao")
+    
+    st.dataframe(merge_matricula_situacao)
+
+    merge_matricula_situacao_notas = pd.merge(notas, merge_matricula_situacao, left_on='situacao_id', right_on='id_situacao', suffixes=('', '_notas'))
+    
+    #Merge final
+    merge_matricula_situacao_notas_disciplina = pd.merge(merge_matricula_situacao_notas, disciplina, left_on='situacao_id', right_on='id', suffixes=('', '_disciplina'))
+
+    #st.dataframe(merge_matricula_situacao_notas_disciplina)
+
 if __name__ == "__main__":
     main()
+
+
