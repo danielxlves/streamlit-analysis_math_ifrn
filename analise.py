c0e5ac33d2f1c686b0394ff7fb30bcffdf5a83bf
danielxlves <daniel.nascimento.702@ufrn.edu.br>
2024-08-27 00:24:53 -0300

    fix, feat: corrigindo merges, add filtros, plot 1º grafico.

--- analise.py
+++ analise.py
@@ -1,10 +1,12 @@
+# %%
 import streamlit as st
 import pandas as pd
-import io
+from IPython.display import display
 import plotly.express as px
 
+# %%
 def load_data():
-    #alunos = pd.read_json('aluno.json')
+    # alunos = pd.read_json('aluno.json')
     matricula = pd.read_json('MatriculaPeriodo.json')
     notas = pd.read_json('Notas.json')
     disciplina = pd.read_json('Disciplina.json')
@@ -12,45 +14,55 @@ def load_data():
 
     return matricula, notas, disciplina, situacao
 
+#  %%
 def main():
-
     matricula, notas, disciplina, situacao = load_data()
 
-    #print("Aluno")
-    #print(alunos)
-    #print("Matricula")
-    #print(matricula)
-    #print("Notas")
-    #print(notas)
-    #print("Disciplinas")
-    #print(disciplina)
-    #print("Situacao")
-    #print(situacao)
-
-    # Merges/Joins
+    #display(matricula.head())
+    #display(notas.head())
+    #display(disciplina.head())
+    #display(situacao.head())
 
     merge_matricula_situacao = pd.merge(matricula, situacao, left_on='situacao_id', right_on='id', suffixes=('_matricula', '_situacao'))
-    
-    print("Merge matricula -> situacao")
-    print(merge_matricula_situacao)
 
-    merge_matricula_situacao_notas = pd.merge(notas, merge_matricula_situacao, left_on='situacao_id', right_on='id_situacao', suffixes=('', '_notas'))
-    
-    print("Notas -> Merge matricula situacao ")
-    print(merge_matricula_situacao_notas)
-    
+    #print("Merge matricula -> situacao")
+    #display(merge_matricula_situacao)
 
-    #Merge final
-    merge_matricula_situacao_notas_disciplina = pd.merge(merge_matricula_situacao_notas, disciplina, left_on='situacao_id', right_on='id', suffixes=('', '_disciplina'))
+    merge_matricula_situacao_notas = pd.merge(notas, merge_matricula_situacao, left_on='matricula_periodo_id', right_on='id_matricula', suffixes=('', '_notas'))
     
-    print("Disciplina -> Merge matricula situacao notas ")
-    print(merge_matricula_situacao_notas_disciplina)
+    #print("Notas -> Merge matricula situacao ")
+    #display(merge_matricula_situacao_notas)
+
+    # Merge final
+    merge_matricula_situacao_notas_disciplina = pd.merge(merge_matricula_situacao_notas, disciplina, left_on='disciplina_id', right_on='id', suffixes=('', '_disciplina'))
+    
+    #print("Disciplina -> Merge matricula situacao notas ")
+    #display(merge_matricula_situacao_notas_disciplina)
+
+    filter_df_final = merge_matricula_situacao_notas_disciplina[merge_matricula_situacao_notas_disciplina['descricao_historico'].str.startswith('Matematica')]
+
+    print("Dados mergado e filtrado")
+    st.dataframe(filter_df_final[["id_matricula","descricao_historico", "ch_hora_relogio", "media_final", "percentual_frequencia"]])
+    
+    import plotly.express as px
+
+    # Criando o gráfico de dispersão interativo
+    fig = px.scatter(
+        filter_df_final, 
+        x='media_final', 
+        y='percentual_frequencia', 
+        text='descricao_historico',  # Adiciona os rótulos das disciplinas
+        title='Relação entre Média Final e Percentual de Frequência',
+        labels={'media_final': 'Média Final', 'percentual_frequencia': 'Percentual de Frequência'},
+        template='plotly_white'
+    )
+
+    # Ajustando a posição dos rótulos para não sobrepor os pontos
+    fig.update_traces(textposition='top center')
+    
+    st.plotly_chart(fig)
+
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-# %%
+#  %%
